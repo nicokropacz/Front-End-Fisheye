35983b093aba12fd426b94e71d72ff5792014347
nico <nicolas.kropacz@gmail.com>
2022-03-30 12:42:33 +0200

    demarage projet

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,31 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+     
+
+        var myHeaders = new Headers();
+
+        var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default',
+             };
+
+        var myRequest = new Request ('./data/photographers.json', myInit);
+
+
+     return fetch (myRequest, myInit).then(data => {
+
+         if(data.ok){
+            console.log(data.body.json()); 
+            return data.json(); 
+
+          }
+   
+        }).then(data => {
+            console.log(data)
+            return data
+        }).catch(error => console.log(error))
+    
     }
 
     async function displayData(photographers) {
